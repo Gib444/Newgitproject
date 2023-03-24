3fc077fddb11d901ff452d154228c0bc7d75492e
hp <hp@DESKTOP-HI8QONG>
2023-03-24 12:06:44 +0530

    1st Test Pass

---
+++ src/main/java/Newgitproject/Newgitproject/App.java
@@ -0,0 +1,13 @@
+package Newgitproject.Newgitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
